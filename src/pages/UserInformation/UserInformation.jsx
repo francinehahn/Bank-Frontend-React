66df621d6d53cf784b6b7d9ddd48cb2872e9f4f3
Francine Hahn <fran_hahn@hotmail.com>
2023-07-15 18:15:05 -0300

    feat: I created the useProtectedPage hook and implemented on the user information page

--- src/pages/UserInformation/UserInformation.jsx
+++ src/pages/UserInformation/UserInformation.jsx
@@ -1,4 +1,8 @@
+import { useProtectedPage } from "../../hooks/useProtectedPage"
+
 export function UserInformation () {
+    useProtectedPage()
+
     return (
         <div>
             Olá, eu sou a página de info do usuario!
